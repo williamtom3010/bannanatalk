914f01a83ac088208a7b06bb8cb4a541dc9cbcf7
williamtom3010 <williamtom45@gmail.com>
2021-07-08 01:38:13 +0530

    jsedit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // var username = prompt("hey gimme ur username");
 // alert("Your username"+username);
 
-var btnTranslate = document.querySelector("#btn-translate")
-console.log(btnTranslate)
+var btnTranslate = document.querySelector("#btn-translate");
+console.log(btnTranslate);
