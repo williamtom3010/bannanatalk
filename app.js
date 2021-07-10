be48b904f0c1cffa90b98e31e91f8f77d0cfaf26
williamtom3010 <williamtom45@gmail.com>
2021-07-10 13:17:07 +0530

    taking input and console

--- app.js
+++ app.js
@@ -2,4 +2,14 @@
 // alert("Your username"+username);
 
 var btnTranslate = document.querySelector("#btn-translate");
-console.log(btnTranslate);
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
+
+// console.log(btnTranslate);
+
+function clickHandler() {
+    console.log("clicked");
+    console.log("text", txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler)
